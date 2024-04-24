12588f0f0ae600a5756237c0c7bd7dae4f907261
Davyd Kennyd <davydkennydtv@gmail.com>
2024-04-24 20:35:39 -0300

    Projeto alterado

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-fetch (` `)
+const apiKey = '162f76918acb4205884f7937002a915c';
+const apiUrl = 'https://newsapi.org/v2/top-headlines';
+const country = 'br';
+
+fetch(`${apiUrl}?country=${country}&apiKey=${apiKey}`)
+    .then(Response => Response.json())
+    .then(data => {
+        const newsConteiner = document.getElementById('news-conteiner');
+
+
+        data.articles.forEach(article => {
+            const articleElement = document.createElement('a');
+            articleElement.href = article.url;
+            articleElement.textContent = article.title;
+            articleElement.classList.add('news-item');
+            newsConteiner.appendChild(articleElement);
+        });
+    })
+    .catch(error => console.error('Erro ao tentar obter notícias.', error));
+
+
