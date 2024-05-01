34ac2e27d97950f1adab4f9d17f1f5bf3b0a7e81
Davyd Kennyd <davydkennydtv@gmail.com>
2024-05-01 14:44:50 -0300

    Alterações feitas

--- script.js
+++ script.js
@@ -1,21 +1,45 @@
-const apiKey = '162f76918acb4205884f7937002a915c';
-const apiUrl = 'https://newsapi.org/v2/top-headlines';
-const country = 'br';
+document.getElementById('pesquisa').addEventListener('submit', async function(event) {
+    event.preventDefault();
 
-fetch(`${apiUrl}?country=${country}&apiKey=${apiKey}`)
-    .then(Response => Response.json())
-    .then(data => {
-        const newsConteiner = document.getElementById('news-conteiner');
+    const cityName = document.getElementById('city_name').value;
 
+    const apiKey = '799b9b8775fd3ffa38fdca0c61a15355';
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${encodeURI(cityName)}&appid=${apiKey}&units=metric&lang=pt_br`;
 
-        data.articles.forEach(article => {
-            const articleElement = document.createElement('a');
-            articleElement.href = article.url;
-            articleElement.textContent = article.title;
-            articleElement.classList.add('news-item');
-            newsConteiner.appendChild(articleElement);
-        });
-    })
-    .catch(error => console.error('Erro ao tentar obter notícias.', error));
+    try {
+        const results = await fetch(apiUrl);
+        const json = await results.json();
 
+        if (json.cod === 200) {
+            showInfo({
+                city: json.name,
+                country: json.sys.country,
+                temp: json.main.temp,
+            });
+        } else {
+            document.querySelector('#weather').classList.remove('show');
+            showAlert(`
+            Não foi possível localizar...
+            
+            <img src="src/images/404.svg" alt="Erro de localização" />
+            `);
+        }
+    } catch (error) {
+        console.error('Erro ao obter dados: ', error);
+        showAlert('Ocorreu um erro ao tentar buscar as informações. Por favor, tente novamente.');
+    }
+});
 
+function showInfo(json) {
+    showAlert('');
+
+    document.querySelector("#weather").classList.add('show');
+
+    document.querySelector('#title').innerHTML = `${json.city}, ${json.country}`;
+
+    document.querySelector('#temp_value').innerHTML = `${json.temp.toFixed(1).toString().replace('.', ',')} <sup>C°</sup>`;
+}
+
+function showAlert(msg) {
+    document.querySelector('.alert').innerHTML = msg;
+}
